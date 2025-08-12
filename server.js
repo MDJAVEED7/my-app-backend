16b4fd74d44db98281d76f7de5708e5ecbc190b9
MDJAVEED7 <hmdjaveedhussain@gmail.com>
2025-08-12 20:06:01 +0530

    Fix backend routes and improve OTP flow

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const hashAadhaar = (aadhaar) => {
 
 const isValidAadhaarFormat = (s) => /^\d{12}$/.test(s);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(()=> console.log('MongoDB connected'))
   .catch(err => { 
     console.error('MongoDB connection error:', err);
